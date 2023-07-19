9c8b1c336b3374f16f26ab06114d26622088a83b
afzal-swe <afzalbhola07@gmail.com>
2023-07-19 06:19:12 +0600

    Make All File in Admin Panel for work

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,104 @@
 <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gentelella Alela! | </title>
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('admin/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{ asset('admin/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{ asset('admin/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
+    <!-- bootstrap-daterangepicker -->
+    <link href="{{ asset('admin/vendors/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="{{ asset('admin/build/css/custom.min.css') }}" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+
+        @auth
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                
+                @include('layouts.partials.sidebar')
+                <!-- /menu footer buttons -->
+                @include('layouts.partials.footer')
+                <!-- /menu footer buttons -->
+                </div>
+            </div>
+            <!-- top navigation -->
+            @include('layouts.partials.topbar')
+            <!-- /top navigation -->
+        @endauth
+        
+
+        <!-- page content -->
+       @yield('content')
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="{{ asset('admin/vendors/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('admin/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('admin/vendors/fastclick/lib/fastclick.js') }}"></script>
+    <!-- NProgress -->
+    <script src="{{ asset('admin/vendors/nprogress/nprogress.js') }}"></script>
+    <!-- Chart.js -->
+    <script src="{{ asset('admin/vendors/Chart.js/dist/Chart.min.js') }}"></script>
+    <!-- jQuery Sparklines -->
+    <script src="{{ asset('admin/vendors/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
+    <!-- Flot -->
+    <script src="{{ asset('admin/vendors/Flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('admin/vendors/Flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('admin/vendors/Flot/jquery.flot.time.js') }}"></script>
+    <script src="{{ asset('admin/vendors/Flot/jquery.flot.stack.js') }}"></script>
+    <script src="{{ asset('admin/vendors/Flot/jquery.flot.resize.js') }}"></script>
+    <!-- Flot plugins -->
+    <script src="{{ asset('admin/vendors/flot.orderbars/js/jquery.flot.orderBars.js') }}"></script>
+    <script src="{{ asset('admin/vendors/flot-spline/js/jquery.flot.spline.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/flot.curvedlines/curvedLines.js') }}"></script>
+    <!-- DateJS -->
+    <script src="{{ asset('admin/vendors/DateJS/build/date.js') }}"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="{{ asset('admin/vendors/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('admin/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="{{ asset('admin/build/js/custom.min.js') }}"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -33,4 +133,4 @@
             </main>
         </div>
     </body>
-</html>
+</html> --}}
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,191 @@
+
+<div class="navbar nav_title" style="border: 0;">
+    <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+  </div>
+
+  <div class="clearfix"></div>
+
+  <!-- menu profile quick info -->
+  <div class="profile clearfix">
+    <div class="profile_pic">
+      <img src="{{ asset('admin/build/images/img.jpg')}}" alt="..." class="img-circle profile_img">
+    </div>
+    <div class="profile_info">
+      <span>Welcome,</span>
+      <h2>John Doe</h2>
+    </div>
+  </div>
+  <!-- /menu profile quick info -->
+
+  <br />
+ 
+ 
+ <!-- sidebar menu -->
+ <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+      <h3>General</h3>
+      <ul class="nav side-menu">
+        <li><a href="{{ route('dashboard') }}" style="font-size: 21px;">Dashboard</a></li>
+
+        <li><a><i class="fa fa-edit"></i> Profile <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">About</a></li>
+            <li><a href="form.html">Update Profile</a></li>
+            <li><a href="form.html">Change Password</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Admin <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="form.html">Add Admin</a></li>
+              <li><a href="form.html">Manage Admin</a></li>
+            </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Account <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+                <li><a href="form.html">Create account</a></li>
+                <li><a href="form.html">Manage account</a></li>
+            </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Libraria <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="form.html">Add Books</a></li>
+              <li><a href="form.html">Manage Books</a></li>
+            </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Teachers <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="form.html">Add Teacher</a></li>
+              <li><a href="form.html">Manage Teachers</a></li>
+            </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Students <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="form.html">Add Student</a></li>
+              <li><a href="form.html">Manage Students</a></li>
+            </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Registration <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="form.html">Student Registration</a></li>
+              <li><a href="form.html">Students Register</a></li>
+            </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Class <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">Create Class</a></li>
+            <li><a href="form.html">Manage Class</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Subject <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">Create Subject</a></li>
+            <li><a href="form.html">Manage Subject</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Exam <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">Create Exam</a></li>
+            <li><a href="form.html">Manage Exam</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Quize <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">Create Quize</a></li>
+            <li><a href="form.html">Manage Quize</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Attendance <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">Add Attendance</a></li>
+            <li><a href="form.html">Manage Attendance</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> Setting <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">Notice</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+          </ul>
+        </li>
+
+        <li><a><i class="fa fa-edit"></i> User Role <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="form.html">SEO</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+            <li><a href="form.html">Events</a></li>
+          </ul>
+        </li>
+
+        
+
+        
+
+       
+
+      </ul>
+    </div>
+
+
+    <div class="menu_section">
+      <h3>Live On</h3>
+      <ul class="nav side-menu">
+        <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="e_commerce.html">E-commerce</a></li>
+            <li><a href="projects.html">Projects</a></li>
+            <li><a href="project_detail.html">Project Detail</a></li>
+            <li><a href="contacts.html">Contacts</a></li>
+            <li><a href="profile.html">Profile</a></li>
+          </ul>
+        </li>
+        <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li><a href="page_403.html">403 Error</a></li>
+            <li><a href="page_404.html">404 Error</a></li>
+            <li><a href="page_500.html">500 Error</a></li>
+            <li><a href="plain_page.html">Plain Page</a></li>
+            <li><a href="login.html">Login Page</a></li>
+            <li><a href="pricing_tables.html">Pricing Tables</a></li>
+          </ul>
+        </li>
+        <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+              <li><a href="#level1_1">Level One</a>
+              <li><a>Level One<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                  <li class="sub_menu"><a href="level2.html">Level Two</a>
+                  </li>
+                  <li><a href="#level2_1">Level Two</a>
+                  </li>
+                  <li><a href="#level2_2">Level Two</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="#level1_2">Level One</a>
+              </li>
+          </ul>
+        </li>                  
+        <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
+      </ul>
+    </div>
+
+  </div>
+  <!-- /sidebar menu -->
