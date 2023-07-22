72d4e06e9918d622cd2e106626864a65dee67141
afzal-swe <afzalbhola07@gmail.com>
2023-07-22 13:24:50 +0600

    Update Admin Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
           <div class="tile-stats">
             <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
+            <h3>Total Students</h3>
             <p>Lorem ipsum psdea itgum rixt.</p>
           </div>
         </div>
@@ -15,7 +15,7 @@
           <div class="tile-stats">
             <div class="icon"><i class="fa fa-comments-o"></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
+            <h3>Total Teachers</h3>
             <p>Lorem ipsum psdea itgum rixt.</p>
           </div>
         </div>
@@ -23,7 +23,7 @@
           <div class="tile-stats">
             <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
+            <h3>Total Books</h3>
             <p>Lorem ipsum psdea itgum rixt.</p>
           </div>
         </div>
@@ -31,7 +31,112 @@
           <div class="tile-stats">
             <div class="icon"><i class="fa fa-check-square-o"></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
+            <h3>Total Users</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row top_tiles">
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
+            <div class="count">179</div>
+            <h3>Departments</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-comments-o"></i></div>
+            <div class="count">179</div>
+            <h3>Student New Register</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
+            <div class="count">179</div>
+            <h3>Total Classes</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-check-square-o"></i></div>
+            <div class="count">179</div>
+            <h3>Total Cource</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row top_tiles">
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
+            <div class="count">179</div>
+            <h3>Students Attendance</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-comments-o"></i></div>
+            <div class="count">179</div>
+            <h3>Teachers Attendance</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
+            <div class="count">179</div>
+            <h3>Libraria Attendance</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-check-square-o"></i></div>
+            <div class="count">179</div>
+            <h3>Total Attendance</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row top_tiles">
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
+            <div class="count">179</div>
+            <h3>Total Payable</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-comments-o"></i></div>
+            <div class="count">179</div>
+            <h3>Total Paid</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
+            <div class="count">179</div>
+            <h3>Total Due</h3>
+            <p>Lorem ipsum psdea itgum rixt.</p>
+          </div>
+        </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-check-square-o"></i></div>
+            <div class="count">179</div>
+            <h3>Extra TK</h3>
             <p>Lorem ipsum psdea itgum rixt.</p>
           </div>
         </div>
@@ -167,293 +272,6 @@
         </div>
       </div>
 
-
-
-      <div class="row">
-        <div class="col-md-12">
-          <div class="x_panel">
-            <div class="x_title">
-              <h2>Weekly Summary <small>Activity shares</small></h2>
-              <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Settings 1</a>
-                    </li>
-                    <li><a href="#">Settings 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
-              </ul>
-              <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
-
-              <div class="row" style="border-bottom: 1px solid #E0E0E0; padding-bottom: 5px; margin-bottom: 5px;">
-                <div class="col-md-7" style="overflow:hidden;">
-                  <span class="sparkline_one" style="height: 160px; padding: 10px 25px;">
-                                <canvas width="200" height="60" style="display: inline-block; vertical-align: top; width: 94px; height: 30px;"></canvas>
-                            </span>
-                  <h4 style="margin:18px">Weekly sales progress</h4>
-                </div>
-
-                <div class="col-md-5">
-                  <div class="row" style="text-align: center;">
-                    <div class="col-md-4">
-                      <canvas class="canvasDoughnut" height="110" width="110" style="margin: 5px 10px 10px 0"></canvas>
-                      <h4 style="margin:0">Bounce Rates</h4>
-                    </div>
-                    <div class="col-md-4">
-                      <canvas class="canvasDoughnut" height="110" width="110" style="margin: 5px 10px 10px 0"></canvas>
-                      <h4 style="margin:0">New Traffic</h4>
-                    </div>
-                    <div class="col-md-4">
-                      <canvas class="canvasDoughnut" height="110" width="110" style="margin: 5px 10px 10px 0"></canvas>
-                      <h4 style="margin:0">Device Share</h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-      <div class="row">
-        <div class="col-md-4">
-          <div class="x_panel">
-            <div class="x_title">
-              <h2>Top Profiles <small>Sessions</small></h2>
-              <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Settings 1</a>
-                    </li>
-                    <li><a href="#">Settings 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
-              </ul>
-              <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item One Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Three Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="x_panel">
-            <div class="x_title">
-              <h2>Top Profiles <small>Sessions</small></h2>
-              <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Settings 1</a>
-                    </li>
-                    <li><a href="#">Settings 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
-              </ul>
-              <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item One Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Three Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="x_panel">
-            <div class="x_title">
-              <h2>Top Profiles <small>Sessions</small></h2>
-              <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Settings 1</a>
-                    </li>
-                    <li><a href="#">Settings 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
-              </ul>
-              <div class="clearfix"></div>
-            </div>
-            <div class="x_content">
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item One Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Two Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-              <article class="media event">
-                <a class="pull-left date">
-                  <p class="month">April</p>
-                  <p class="day">23</p>
-                </a>
-                <div class="media-body">
-                  <a class="title" href="#">Item Three Title</a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 @endsection
