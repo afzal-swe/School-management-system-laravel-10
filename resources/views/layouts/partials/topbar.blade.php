6b811dc6952edb77ecf808171b3160d99472013c
afzal-swe <afzalbhola07@gmail.com>
2023-07-29 08:02:32 +0600

    Update Sidebar and topbar Auth user name

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -9,7 +9,7 @@
         <ul class="nav navbar-nav navbar-right">
           <li class="">
             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-              <img src="{{ asset ('admin/build/images/img.jpg')}}" alt="">John Doe
+              <img src="{{ asset ('admin/build/images/img.jpg')}}" alt="">{{ Auth::user()->name }}
               <span class=" fa fa-angle-down"></span>
             </a>
             <ul class="dropdown-menu dropdown-usermenu pull-right">
